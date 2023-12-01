2d2d65e8302e15dda836cd35d179a380734b35e1
Carlos Henrique <97392902+kralluz@users.noreply.github.com>
2023-12-01 11:15:19 -0300

    Update fila.js

--- exercicio de fila/fila.js
+++ exercicio de fila/fila.js
@@ -1,45 +1,68 @@
-const fila = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 const readline = require('readline');
+
+class Queue {
+    constructor() {
+        this.items = [];
+    }
+
+    enqueue(item) {
+        this.items.push(item);
+    }
+
+    dequeue() {
+        if (this.isEmpty()) {
+            return "A fila está vazia.";
+        }
+        return this.items.shift();
+    }
+
+    isEmpty() {
+        return this.items.length === 0;
+    }
+
+    viewQueue() {
+        return "Fila atual: " + this.items.join(", ");
+    }
+}
+
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
-console.log("Opção 1: ver fila.");
-console.log("Opção 2: adicionar ao final da fila.");
-console.log("Opção 3: remover primeiro da fila.");
-function mostrarFila() {
-    console.log(fila);
-}
-function adicionarFila() {
-    rl.question("Digite o numero a ser adidionado \n", (response) =>{
-        fila.push(parseInt(response));
-        console.log(fila);
-        rl.close()
-    }
-)}
-function removerFila() {
-    fila.shift();
-    console.log(fila);
-    rl.close();
-}
-function perguntarOpcao() {
-    rl.question("Digite a opção (1, 2 ou 3): ", (response) => {
-        const opcao = parseInt(response);
-        switch (opcao) {
-            case 1:
-                mostrarFila();
+
+const queue = new Queue();
+
+const menu = () => {
+    console.log("\n=== Menu da Fila ===");
+    console.log("1. Visualizar fila");
+    console.log("2. Adicionar item à fila");
+    console.log("3. Remover item da fila");
+    console.log("4. Sair");
+    rl.question("Escolha uma opção: ", (option) => {
+        switch(option) {
+            case '1':
+                console.log(queue.viewQueue());
+                menu();
                 break;
-            case 2:
-                adicionarFila();
+            case '2':
+                rl.question("Digite o item para adicionar: ", (item) => {
+                    queue.enqueue(item);
+                    console.log(`'${item}' adicionado à fila.`);
+                    menu();
+                });
                 break;
-            case 3:
-                removerFila();
+            case '3':
+                console.log(queue.dequeue());
+                menu();
+                break;
+            case '4':
+                rl.close();
                 break;
             default:
-                console.log("Opção inválida. Por favor, escolha 1, 2 ou 3.");
+                console.log("Opção inválida.");
+                menu();
         }
-
-        perguntarOpcao();
     });
-}
-perguntarOpcao(); 
+};
+
+menu();
